79d96ee6ea8b3294d32517291a177ca8bf2b3051
Ganeshsabbani <ganeshsabbani63@gmail.com>
2022-07-11 21:43:49 +0530

    first Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./bootstrap.min.css"
+import App from './App';
+import "./App.css"
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
